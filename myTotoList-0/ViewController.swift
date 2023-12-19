4ec65ee13f1377e8ca3d56cb431737eb4d673de0
Jooyeong(extrayugen) <supershicker@gmail.com>
2023-12-19 16:28:07 +0900

    Initial Commit

---
+++ myTotoList-0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTotoList-0
+//
+//  Created by t2023-m0031 on 12/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
